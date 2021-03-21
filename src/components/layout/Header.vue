76d7d4c9a5a7a2ade0dde41697e8ac7b93a2e22d
Mathieu Ibersien <mathieu.ibersien@etu.univ-lehavre.fr>
2021-03-21 10:53:57 +0100

    Header liens refait

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,9 +1,9 @@
 <template >
     <div class="header">
         <div class="nav-bar">
-            <router-link to="/">Home</router-link>
-            <router-link to="/todos">Todos</router-link>
-            <router-link to="/sliders">Sliders</router-link>
+            <router-link class="nav-link" to="/">Home</router-link>
+            <router-link class="nav-link" to="/todos">Todos</router-link>
+            <router-link class="nav-link" to="/sliders">Sliders</router-link>
         </div>
         <img alt="vue js logo" src="../../assets/images/vue_logo.png"/>
     </div>
@@ -17,13 +17,37 @@ export default {
     .header {
         text-align: center;
     }
+
     .nav-bar {
+        margin-top: 30px;
         display: flex;
         justify-content: space-around;
     }
 
-    .nav-bar a {
-        font-size: 25px
+    .nav-link {
+        position: relative;
+        font-family: sans-serif;
+        font-size: 20px;
+        text-decoration: none;
+        text-transform: uppercase;
+        color: #222;
+    }
+
+    .nav-link::before {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        background: #222;
+        height: 3px;
+        width: 100%; /* taille de la barre*/
+        transform-origin: 120% 0; /* distance du point de chute */   
+        transform: scaleX(0);
+        transition: transform 0.4s;
+    }
+
+    .nav-link:hover::before {
+        transform-origin: 0 0;
+        transform: scaleX(1);
     }
 
     img {
