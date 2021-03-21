c3401edc2a6ae94bc52c8f3fade68e5ccb3978b5
Mathieu Ibersien <mathieu.ibersien@etu.univ-lehavre.fr>
2021-03-21 18:58:11 +0100

    Nav link from "les designers du web"

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,9 +1,12 @@
 <template >
     <div class="header">
         <div class="nav-bar">
-            <router-link class="nav-link" to="/">Home</router-link>
-            <router-link class="nav-link" to="/todos">Todos</router-link>
-            <router-link class="nav-link" to="/sliders">Sliders</router-link>
+            <router-link class="nav-link-1" to="/">Home</router-link>
+            <router-link class="slide-link" to="/todos">
+                <span>Todos</span>
+                <span>Todos</span>
+            </router-link>
+            <router-link class="nav-link-3" to="/sliders">Sliders</router-link>
         </div>
         <img alt="vue js logo" src="../../assets/images/vue_logo.png"/>
     </div>
@@ -18,13 +21,19 @@ export default {
         text-align: center;
     }
 
+    img {
+        margin: 100px auto 0 auto;
+    }
+    
     .nav-bar {
         margin-top: 30px;
         display: flex;
         justify-content: space-around;
     }
 
-    .nav-link {
+/** COUNTER */
+
+    .nav-link-1 {
         position: relative;
         font-family: sans-serif;
         font-size: 20px;
@@ -33,7 +42,7 @@ export default {
         color: #222;
     }
 
-    .nav-link::before {
+    .nav-link-1::before {
         content: "";
         position: absolute;
         bottom: 0;
@@ -45,12 +54,84 @@ export default {
         transition: transform 0.4s;
     }
 
-    .nav-link:hover::before {
+    .nav-link-1:hover::before {
         transform-origin: 0 0;
         transform: scaleX(1);
     }
 
-    img {
-        margin: 100px auto 0 auto;
+/** TODOS */
+
+.slide-link {
+    font-family: sans-serif;
+    overflow: hidden;
+    position: relative;
+       font-size: 20px;
+        text-decoration: none;
+        text-transform: uppercase;
+        color: #222;
+}
+
+.slide-link span {
+    padding: 0 10px;
+    display: inline-block;/* De base les spans sont des elements inline ici on met au moins block en plus pour pouvoir y ajouter une transformation */
+    transition: transform 0.3s ease-out; 
+}
+
+.slide-link span:nth-child(2){
+    position: absolute;
+    width: 100%;
+    text-align: center;
+    left: 100%;
+}
+
+.slide-link:hover span {
+    transform: translateX(-100%);
+}
+
+/** SLIDERS */
+
+    .nav-link-3 {
+        font-size: 20px;
+        text-decoration: none;
+        text-transform: uppercase;
+        color: #222;
+        position: relative;
+    }
+
+    .nav-link-3::before,
+    .nav-link-3::after {
+        content: "";
+        position: absolute;
+        height: 2px;
+        opacity: 0;
+        background: #222;
+        left: 50%;
+        transform: translate(-50%, 10px);
+        transition: all 0.2s ease-in-out;
+    }
+
+    .nav-link-3::before {
+        width: 90%;
+        bottom: 0;;
+    }
+
+    .nav-link-3::after {
+        width: 60%;
+        bottom: -7px;
+    }
+
+    .nav-link-3:hover::before,
+    .nav-link-3:hover::after {
+        opacity: 1;
+        transform: translate(-50%, 0px);
+    }
+
+    .nav-link-3::before, /** ajoute un delay quand on enlève la souris de notre lien sur la barre du haut */
+    .nav-link-3:hover::after { /**ajoute un delay pour la barre du bas */
+        transition-delay: 0.1s;
+    }
+
+    .nav-link-3:hover::before { /** enlève le delay pour pour la barre du haut pour le hover */
+        transition-delay: 0s;
     }
 </style>
